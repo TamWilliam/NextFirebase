9e28b894dcce55585aaf5da31cf638a0cee1088d
Pierre Leroy <pleroy0307@gmail.com>
2024-02-14 14:14:47 +0100

    correction import + create signin page

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,9 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { useEffect } from "react";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-import { auth } from "./firebase";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  useEffect(() => {
-    createUserWithEmailAndPassword(
-      auth,
-      "pleroy030@gmail.com",
-      "password"
-    ).then((response) => {
-      console.log(response);
-    });
-
-    return () => {};
-  }, []);
 
   return (
     <main
