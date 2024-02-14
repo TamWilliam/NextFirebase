ebdf457787592519d28f515b6b6b4d843fad4c65
William Tam <william.tam@supdevinci-edu.fr>
2024-02-14 15:12:43 +0100

    Fix css

--- pages/login.js
+++ pages/login.js
@@ -1,77 +1,84 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
-import { Inter } from "next/font/google";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "./firebase";
-import Link from 'next/link';
+import { useState } from "react"
+import { useRouter } from "next/router"
+import { Inter } from "next/font/google"
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "./firebase"
+import Link from "next/link"
 
-import "tailwindcss/tailwind.css";
+import "tailwindcss/tailwind.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export default function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
-  const router = useRouter();
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(null)
+  const router = useRouter()
 
   const handleSignIn = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     try {
-      const response = await signInWithEmailAndPassword(auth, email, password);
-      console.log(response);
+      const response = await signInWithEmailAndPassword(auth, email, password)
+      console.log(response)
 
       /* redirection sur index.js */
-      router.push("/");
+      router.push("/")
     } catch (error) {
-      console.error(error.code, error.message);
-      setError("Mail ou mot de passe incorrect.");
+      console.error(error.code, error.message)
+      setError("Mail ou mot de passe incorrect.")
     }
-  };
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1>Se connecter</h1>
-      <form onSubmit={handleSignIn} className="max-w-md p-4 bg-white shadow-md rounded-md">
-        <label className="block mb-2 text-sm font-medium text-gray-600">
-          Adresse mail :
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full px-3 py-2 border rounded-md"
-            required
-          />
-        </label>
-
-        <label className="block mb-2 text-sm font-medium text-gray-600">
-          Mot de passe :
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-3 py-2 border rounded-md"
-            required
-          />
-        </label>
-
-        {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
-
-        <button
-          type="submit"
-          className="w-full mt-4 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
+      <div className="max-w-md p-4 bg-white shadow-md rounded-md">
+        <h1 className="text-center text-3xl font-semibold text-gray-800 mb-4">
+          Se connecter
+        </h1>
+        <form
+          onSubmit={handleSignIn}
+          className="max-w-md p-4 bg-white shadow-md rounded-md"
         >
-          Connexion
-        </button>
+          <label className="block mb-2 text-sm font-medium text-gray-600">
+            Adresse mail :
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-3 py-2 border rounded-md"
+              required
+            />
+          </label>
 
-        <p className="text-gray-600 text-sm mt-2">
-          Pas de compte ?{" "}
-          <Link legacyBehavior href="/register">
-            <a style={{ textDecoration: 'underline' }}>Créer un compte</a>
-          </Link>
-        </p>
-      </form>
+          <label className="block mb-2 text-sm font-medium text-gray-600">
+            Mot de passe :
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-3 py-2 border rounded-md"
+              required
+            />
+          </label>
+
+          {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
+
+          <button
+            type="submit"
+            className="w-full mt-4 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
+          >
+            Connexion
+          </button>
+
+          <p className="text-gray-600 text-sm mt-2">
+            Pas de compte ?{" "}
+            <Link legacyBehavior href="/register">
+              <a style={{ textDecoration: "underline" }}>Créer un compte</a>
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
--- pages/register.js
+++ pages/register.js
@@ -1,20 +1,17 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
-import { Inter } from "next/font/google";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "./firebase";
+import { useState } from "react"
+import { useRouter } from "next/router"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { auth } from "./firebase"
 
-import "tailwindcss/tailwind.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import "tailwindcss/tailwind.css"
 
 export default function Register() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const router = useRouter();
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const router = useRouter()
 
   const handleSignUp = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     try {
       /* authentification réussie */
@@ -22,50 +19,52 @@ export default function Register() {
         auth,
         email,
         password
-      );
-      console.log(response);
-      router.push("/login");
-    } catch (error) { /* affichage de l'erreur */
-      console.error(error.code, error.message);
+      )
+      console.log(response)
+      router.push("/login")
+    } catch (error) {
+      /* affichage de l'erreur */
+      console.error(error.code, error.message)
     }
-  };
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1>Créer son compte</h1>
-      <form
-        onSubmit={handleSignUp}
-        className="max-w-md p-4 bg-white shadow-md rounded-md"
-      >
-        <label className="block mb-2 text-sm font-medium text-gray-600">
-          Adresse mail :
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full px-3 py-2 border rounded-md"
-            required
-          />
-        </label>
+      <div className="max-w-md p-4 bg-white shadow-md rounded-md">
+        <h1 className="text-center text-3xl font-semibold text-gray-800 mb-4">
+          Créer son compte
+        </h1>
+        <form onSubmit={handleSignUp}>
+          <label className="block mb-2 text-sm font-medium text-gray-600">
+            Adresse mail :
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-3 py-2 border rounded-md"
+              required
+            />
+          </label>
 
-        <label className="block mb-2 text-sm font-medium text-gray-600">
-          Mot de passe :
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-3 py-2 border rounded-md"
-            required
-          />
-        </label>
+          <label className="block mb-2 text-sm font-medium text-gray-600">
+            Mot de passe :
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-3 py-2 border rounded-md"
+              required
+            />
+          </label>
 
-        <button
-          type="submit"
-          className="w-full mt-4 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
-        >
-          S'enregistrer
-        </button>
-      </form>
+          <button
+            type="submit"
+            className="w-full mt-4 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
+          >
+            S'enregistrer
+          </button>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
