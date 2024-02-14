89e864cc37fb24b1b32f2c185b0cbf2259857525
Pierre Leroy <pleroy0307@gmail.com>
2024-02-14 14:41:46 +0100

    create signup.js

--- pages/signin.js
+++ pages/signin.js
@@ -1,40 +1,38 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 import { Inter } from "next/font/google";
-import { createUserWithEmailAndPassword } from "firebase/auth"; /* créer un utilisateur */
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./firebase";
+import Link from 'next/link';
 
 import "tailwindcss/tailwind.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function SignIn() {
-  /* état local */
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const router = useRouter();
 
-  /* gestion de la soumission de la création du form */
-  const handleSignUp = async (e) => {
+  const handleSignIn = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
+      const response = await signInWithEmailAndPassword(auth, email, password);
       console.log(response);
+
+      /* redirection sur index.js */
       router.push("/");
     } catch (error) {
       console.error(error.code, error.message);
+      setError("Mail ou mot de passe incorrect.");
     }
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <form
-        onSubmit={handleSignUp}
-        className="max-w-md p-4 bg-white shadow-md rounded-md"
-      >
+      <form onSubmit={handleSignIn} className="max-w-md p-4 bg-white shadow-md rounded-md">
         <label className="block mb-2 text-sm font-medium text-gray-600">
           Adresse mail :
           <input
@@ -57,12 +55,21 @@ export default function SignIn() {
           />
         </label>
 
+        {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
+
         <button
           type="submit"
           className="w-full mt-4 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
         >
-          S'enregistrer
+          Connexion
         </button>
+
+        <p className="text-gray-600 text-sm mt-2">
+          Pas de compte ?{" "}
+          <Link legacyBehavior href="/signup">
+            <a style={{ textDecoration: 'underline' }}>Créer un compte</a>
+          </Link>
+        </p>
       </form>
     </div>
   );
