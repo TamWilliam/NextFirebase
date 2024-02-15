89d50894d9e2407fcfbf60ba66834c9d42c93131
William Tam <william.tam@supdevinci-edu.fr>
2024-02-15 16:49:19 +0100

    fix footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-gray-800 p-4 absolute bottom-0 w-full">
       <div className="container mx-auto flex justify-between items-center">
         <div>
-          <span className="text-white">© 2024 Mon Site</span>
+          <span className="text-white">© 2024 Les Ws</span>
         </div>
         <div>
           <span className="text-white p-4">Contact</span>
